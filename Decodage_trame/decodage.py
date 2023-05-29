5d79a123bfbd0eb8deed04a174703f5a957b5a5a
Kisobu <gharach.jawad@gmail.com>
2023-05-29 21:54:44 +0200

    version finale

--- Decodage_trame/decodage.py
+++ Decodage_trame/decodage.py
@@ -60,13 +60,4 @@ for trame in trames:
 		val_arp = trame + (1,)
 		cursor.execute(req_arp, val_arp) 
 
-for trame in trames:		
-	if trame[7] == "0x800":
-		req_udp = "INSERT INTO trame (framedate, MID, bench_3, bench_5, framesize, mac_dest, mac_src, field_1, field_2, field_3, field_4, field_5, field_6, field_7, src_ip, dest_ip, field_9, field_10, field_11, field_14, field_16, field_17, field_18, field_20, field_21, field_23, field_25, field_26, field_28, field_29, field_30, field_32, packet_date, num_test) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s,%s)"
-		val_udp = trame + (2,)
-		cursor.execute(req_udp, val_udp)
-	elif trame[6] == "0x806":
-		req_arp = "INSERT INTO trame (framedate, bench_3, bench_5, framesize, mac_dest, mac_src, field_1, field_2, field_3 , field_4, field_5 , field_6, mac_sender,sender_ip ,mac_target,target_ip, num_test) VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-		val_arp = trame + (2,)
-		cursor.execute(req_arp, val_arp)
 #db.commit()

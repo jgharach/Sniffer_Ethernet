abfca79eace55fec79e997edd085fdd4c586b43b
gj200498 <gharach.jawad@gmail.com>
2023-06-13 15:27:18 +0200

    V1.2

--- affichage_trame.php
+++ affichage_trame.php
@@ -2,19 +2,24 @@
 <html lang="fr"> 
 <head> 
     <meta charset="utf-8" />
-    <title>Thales</title> 
+    <title>SAE23</title> 
 </head> 
 <body>
     <?php
     include('base.php');
-    $sql = "SELECT framedate, MID, bench_3, bench_5, framesize, mac_dest, mac_src, field_1, field_2, field_3 , field_4 , field_5 , field_6 , field_7 , src_ip, dest_ip, field_9, field_10, field_11, field_14, field_16 , field_17, field_18, field_20 , field_21 , field_23, field_25 , field_26 , field_28 , field_29 , field_30 ,field_32, packet_date, mac_sender,sender_ip ,mac_target,target_ip FROM trame";
+    $test = $_GET['test'];
+    $date = $_GET['date'];
+    $sql = "SELECT id_test FROM test WHERE nom_test = '$test' AND date_execution = '$date'";
     $req = $bd->prepare($sql);
     $req->execute();
-    $trame = $req->fetchall(PDO::FETCH_ASSOC);
+    $id = $req->fetchall(PDO::FETCH_ASSOC);
     $req->closeCursor();
-    foreach ($trame as $val){
-        echo "$trame";
-    }
+    echo "{$id['id_test']}";
+   // $sql = "SELECT framedate, MID, bench_3, bench_5, framesize, mac_dest, mac_src, field_1, field_2, field_3 , field_4 , field_5 , field_6 , field_7 , src_ip, dest_ip, field_9, field_10, field_11, field_14, field_16 , field_17, field_18, field_20 , field_21 , field_23, field_25 , field_26 , field_28 , field_29 , field_30 ,field_32, packet_date, mac_sender,sender_ip ,mac_target,target_ip FROM trame";
+    //$req = $bd->prepare($sql);
+    //$req->execute();
+    //$trame = $req->fetchall(PDO::FETCH_ASSOC);
+   // $req->closeCursor();
     ?> 
 </body> 
  </html>
--- base.php
+++ base.php
@@ -1,6 +1,6 @@
 <?php
 try {
-        $bd = new PDO ("mysql:host=localhost;dbname=thales"/*identifiant isis*/,"root"/*identifiant isis*/,"brKMEbR?f@hLY885"/*mot de passe de la base perso*/);
+        $bd = new PDO ("mysql:host=localhost;dbname=gj200498"/*identifiant isis*/,"gj200498"/*identifiant isis*/,"gj2004987503"/*mot de passe de la base perso*/);
         $bd->exec('SET NAMES utf8');
 
 } catch (Exception $e) {

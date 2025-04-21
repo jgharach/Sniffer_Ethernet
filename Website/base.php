4f99d6c608d531d76bcb2e2ad2871199d816123b
jgharach <69178823+jgharach@users.noreply.github.com>
2025-04-21 17:57:10 +0200

    Update base.php

--- Website/base.php
+++ Website/base.php
@@ -1,6 +1,6 @@
 <?php
 try {
-        $bd = new PDO ("mysql:host=localhost;dbname=thales","root","brKMEbR?f@hLY885");
+        $bd = new PDO ("mysql:host=localhost;dbname=thales","root","password");
         $bd->exec('SET NAMES utf8');
 
 } catch (Exception $e) {
